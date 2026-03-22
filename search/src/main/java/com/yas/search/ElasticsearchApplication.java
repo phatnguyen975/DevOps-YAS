c65efa51d690fa71570c81364a81947e61983aa9
Tipie <tiennt220805@gmail.com>
2026-03-22 23:41:50 +0700

    ci(search): Open pull request for Seacrh service

--- search/src/main/java/com/yas/search/ElasticsearchApplication.java
+++ search/src/main/java/com/yas/search/ElasticsearchApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration;
 
 @EnableConfigurationProperties(ServiceUrlConfig.class)
-@SpringBootApplication(scanBasePackages = {"com.yas.search", "com.yas.commonlibrary"})
+@SpringBootApplication(scanBasePackages = { "com.yas.search", "com.yas.commonlibrary" })
 @Configuration
 public class ElasticsearchApplication {
 
