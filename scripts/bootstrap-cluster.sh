18a54da280991a6ed0f02b31c3d3b01ed2d4e512
Nguyễn Tấn Phát <phatnguyen9725@gmail.com>
2026-04-19 22:00:58 +0700

    Feature/cd (#47)
    * chore: update Jenkinsfile
    
    * feat: add boostrap script to setup dependencies for cluster

---
+++ scripts/bootstrap-cluster.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+helm repo add postgres-operator-charts https://opensource.zalando.com/postgres-operator/charts/postgres-operator
+helm repo add strimzi https://strimzi.io/charts/
+helm repo add elastic https://helm.elastic.co
+helm repo add jetstack https://charts.jetstack.io
+helm repo add stakater https://stakater.github.io/stakater-charts
+helm repo update
+
+helm upgrade --install postgres-operator postgres-operator-charts/postgres-operator \
+  --namespace postgres-operator \
+  --create-namespace
+
+helm upgrade --install kafka-operator strimzi/strimzi-kafka-operator \
+  --namespace kafka-operator \
+  --create-namespace
+
+helm upgrade --install elastic-operator elastic/eck-operator \
+  --namespace elastic-system \
+  --create-namespace
+
+helm upgrade --install cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --create-namespace \
+  --version v1.12.0 \
+  --set installCRDs=true \
+  --set prometheus.enabled=false \
+  --set webhook.timeoutSeconds=4 \
+  --set admissionWebhooks.certManager.create=true
+
+helm upgrade --install reloader stakater/reloader \
+  --namespace reloader \
+  --create-namespace
+
+kubectl create namespace keycloak
+kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.0.2/kubernetes/keycloaks.k8s.keycloak.org-v1.yml
+kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.0.2/kubernetes/keycloakrealmimports.k8s.keycloak.org-v1.yml
+kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.0.2/kubernetes/kubernetes.yml -n keycloak
