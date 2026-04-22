a20206d08fb8912e2d5c111e171ac141a0807559
Phat Nguyen <phatnguyen9725@gmail.com>
2026-04-22 09:45:03 +0700

    chore: update bootstrap-cluster script

--- scripts/bootstrap-cluster.sh
+++ scripts/bootstrap-cluster.sh
@@ -5,7 +5,11 @@ echo "Bootstrapping cluster with operators and CRDs"
 
 helm repo add postgres-operator-charts https://opensource.zalando.com/postgres-operator/charts/postgres-operator
 helm repo add strimzi https://strimzi.io/charts/
+helm repo add akhq https://akhq.io/
 helm repo add elastic https://helm.elastic.co
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
 helm repo add jetstack https://charts.jetstack.io
 helm repo add stakater https://stakater.github.io/stakater-charts
 helm repo update
@@ -37,7 +41,29 @@ helm upgrade --install reloader stakater/reloader \
   --namespace reloader \
   --create-namespace
 
+helm upgrade --install opentelemetry-operator open-telemetry/opentelemetry-operator \
+  --namespace observability-operators \
+  --create-namespace
+
+helm upgrade --install prometheus-operator-crds prometheus-community/prometheus-operator-crds \
+  --namespace observability-operators \
+  --create-namespace
+
+helm upgrade --install grafana-operator oci://ghcr.io/grafana-operator/helm-charts/grafana-operator \
+  --version v5.0.2 \
+  --namespace observability-operators \
+  --create-namespace
+
 kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.0.2/kubernetes/keycloaks.k8s.keycloak.org-v1.yml
 kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.0.2/kubernetes/keycloakrealmimports.k8s.keycloak.org-v1.yml
+kubectl create namespace keycloak --dry-run=client -o yaml | kubectl apply -f -
+kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.0.2/kubernetes/kubernetes.yml -n keycloak
+
+kubectl create clusterrolebinding keycloak-operator-global-admin \
+  --clusterrole=cluster-admin \
+  --serviceaccount=keycloak:keycloak-operator \
+  --dry-run=client -o yaml | kubectl apply -f -
+
+kubectl set env deployment/keycloak-operator WATCH_NAMESPACE="" -n keycloak
 
 echo "Cluster bootstrapped successfully"
